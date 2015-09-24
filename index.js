177b9ae71eb6ca64c6676431024c8f0548247347
Steve Sartino <stevesartino@yahoo.com>
2015-09-24 17:54:48 -0500

    Initial web site working.

---
+++ index.js
@@ -0,0 +1,61 @@
+/* Require node modules to implement */
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var app = require('./server/app.js');
+
+/*createServer takes in a function to handle requests.
+Here is where you can create a handler for get and post requests.
+Note: req(request) and res(response) come from node's http module.
+They include both incoming information like urls and outgoing like content */
+
+/*In order to complete the project, this callback will need to handle
+get requests, post requests and server up other files like css.
+
+Hint: creating a function to replace the anonymous function may be useful.
+		EXAMPLE:
+		function requestHandler(req, res) {
+			if(request url === '/'){
+				// handle this way
+			} else if(request url === '/messages'){
+				if(request method  === 'GET'){
+					// handle this way
+				}
+
+				...
+		};
+
+		var server = http.createServer(requestHandler)
+
+//var server = http.createServer(function(req, res){
+
+/*
+	/* file path to the index.html file using path module */
+/*	var file = path.join(__dirname, './client/index.html');
+
+	  fs.readFile(file, function (err, data) {
+		var statusCode = statusCode || 200;
+		var header = {
+			//Content-Type will need to be changed if it is something other than html, like: js,css or json
+			'Content-Type': 'text/html'
+		};
+
+		/* writeHead writes a response which includes statusCodes and header*/
+	//	res.writeHead(statusCode, header);
+
+		/*indicates conclusion of the response https://nodejs.org/api/http.html#http_response_end_data_encoding_callback*/
+	//	res.end(data);
+//	  });
+
+/*Requests are completed over port numbers and each one needs a unique number. Most port numbers between 0 - 1024 are reserved so here 3000 is a standard testing port to use. More on port numbers: https://en.wikipedia.org/wiki/Port_(computer_networking) */
+var server = http.createServer(app.handleRequests);
+var port = 3000;
+
+server.listen(port);
+console.log('Listening on port', port);
+
+
+
+
+
---
+++ server/app.js
@@ -0,0 +1,116 @@
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+
+function getFileExtension(route) {
+    var arr = route.split('.');
+    if(arr.length<=1) {
+        return 'html'
+    }
+    return arr[arr.length -1].toLowerCase();
+}
+
+function handleRequests(req, res) {
+    var header;
+    var route = url.parse(req.url).path;
+    if(route === '/') {
+        route = '/index.html';
+    }
+    var ext = getFileExtension(route);
+    switch (ext) {
+        case 'css':
+            header = {
+                'Content-Type': 'text/css'
+            };
+            break;
+        case 'js':
+            header = {
+                'Content-Type': 'application/javascript'
+            };
+            break;
+        case 'html':
+            header = {
+                'Content-Type': 'text/html'
+            };
+            break;
+        default:
+            header = {
+                'Content-Type': 'application/json'
+            };
+            break;
+    }
+    
+    var filePath;
+    if(route === '/messages') {
+        if(req.method === 'GET') {
+            filePath = './messages.txt';
+            readFile(filePath, header, res);
+        }
+        if(req.method === 'POST') {
+            var body = '';
+            req.on('data', function (data) {
+                body += data;
+            });
+            req.on('end', function () {
+                writeTweet(JSON.parse(body), res);
+            });
+        }
+    } else {
+        filePath = '../client' + route;
+        readFile(filePath, header, res);
+    }
+}
+
+function writeTweet(tweet, res) {
+    if(tweet.text && tweet.userName) {
+        console.log(tweet);
+        fs.appendFile(__dirname + '/messages.txt', JSON.stringify(tweet), function(err){
+            console.log(err);
+            res.writeHead(200);
+            res.end();
+        });
+    } else {
+        console.log('server error');
+        res.writeHead(500);
+        res.end();
+    }
+}
+
+function readFile(filePath, header, res) {
+    
+    var file = path.join(__dirname, filePath);
+      fs.readFile(file, function (err, data) {
+        var statusCode = statusCode || 200;
+        res.writeHead(statusCode, header);
+        res.end(data);
+      });
+}
+
+module.exports = {
+    handleRequests: handleRequests
+};
+
+//create module to handle routes
+/*createServer takens in a function to handle requests. Here is where you can
+create a handler for get and post requests. Note: req(request) and res(response)
+come from node's http module. They include both incoming information like urls
+and outgoing like content */
+
+/*In order to complete the project, this callback will need to handle get requests,
+post requests and server up other files like css.
+
+Hint: creating a function to replace the anonymous function may be useful.
+		EXAMPLE:
+		function requestHandler(req, res) {
+			if(request url === '/'){
+				// handle this way
+			} else if(request url === '/messages'){
+				if(request method  === 'GET'){
+					// handle this way
+				}
+
+				...
+		};
+
+		var server = http.createServer(requestHandler)
+*/
